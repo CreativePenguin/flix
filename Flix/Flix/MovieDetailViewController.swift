7407198ee591d1518d6b7354cf47244d06b0ec67
Winston Peng <panda501wp@gmail.com>
2022-02-15 22:36:03 -0500

    Part 1 is now working

--- Flix/Flix/MovieDetailViewController.swift
+++ Flix/Flix/MovieDetailViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 class MovieDetailViewController: UIViewController {
     
     var movie: [String: Any]!
-
+    
+    @IBOutlet weak var moviePfp: UIImageView!
+    @IBOutlet weak var movieBackgroundPic: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         print(movie!)
+        self.titleLabel.text = movie["title"] as? String
+        self.descLabel.text = movie["overview"] as? String
+        self.dateLabel.text = movie["release_date"] as? String
+        let baseUrl = "https://image.tmdb.org/t/p/w"
+        let pfpUrl = URL(string: baseUrl + "185" +  (movie["poster_path"] as! String))
+        let bgUrl = URL(string: baseUrl + "780" +  (movie["backdrop_path"] as! String))
+        self.movieBackgroundPic.af.setImage(withURL: bgUrl!)
+        self.moviePfp.af.setImage(withURL: pfpUrl!)
         // Do any additional setup after loading the view.
     }
     
