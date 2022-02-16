00e97f0ea509ef1c79f43feaa105a99d54855083
Winston Peng <panda501wp@gmail.com>
2022-02-15 23:05:38 -0500

    Finalize part 1 & begin part 2 Grids are so hard and dumb. Even texas can't get thheir power grid right and they're like geniuses or something

---
+++ Flix/Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Winston Peng on 2/15/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var moviePfp: UIImageView!
+}
---
+++ Flix/Flix/MovieGridViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MovieGridViewController.swift
+//  Flix
+//
+//  Created by Winston Peng on 2/15/22.
+//
+
+import UIKit
+
+class MovieGridViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+
+}
